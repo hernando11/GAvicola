47e2a670f7c29cfba1851ef77883b819fe0fbc60
hernando11 <hernandogps@gmail.com>
2021-10-11 16:58:31 -0500

    BD- Migracion Ver12

--- GAvicola.App.Persistencia/Migrations/20211011212013_Version12.Designer.cs
+++ GAvicola.App.Persistencia/Migrations/20211011212013_Version12.Designer.cs
@@ -4,14 +4,16 @@ using GAvicola.App.Persistencia;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GAvicola.App.Persistencia.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20211011212013_Version12")]
+    partial class Version12
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GAvicola.App.Persistencia/Migrations/20211011212013_Version12.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GAvicola.App.Persistencia.Migrations
+{
+    public partial class Version12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GAmbientes_Galpones_OperarioId",
+                table: "GAmbientes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GAmbientes_Personas_OperarioId",
+                table: "GAmbientes",
+                column: "OperarioId",
+                principalTable: "Personas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_GAmbientes_Personas_OperarioId",
+                table: "GAmbientes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_GAmbientes_Galpones_OperarioId",
+                table: "GAmbientes",
+                column: "OperarioId",
+                principalTable: "Galpones",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
